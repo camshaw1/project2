b2cae6c05972e47022fb92c1ab88d81e4de5c0cc
camshaw1 <cameronshaw95@hotmail.com>
2016-05-02 04:57:29 +1200

    oldmap

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,76 @@
-var mymap = L.map('leafletmap');
-mymap.setView([40.775,-73.972], 15);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18
-}).addTo(mymap);
+/** NOTE: uses jQuery for quick & easy DOM manipulation **/
 
-$('.pure-button').on('click', function(){
-  mymap.locate({setView: true, maxZoom: 15});
+function getLocation() {
+  var msg;
+
+  /** 
+  first, test for feature support
+  **/
+  if ('geolocation' in navigator) {
+    // geolocation is supported :)
+    requestLocation();
+  } else {
+    // no geolocation :(
+    msg = "Sorry, looks like your browser doesn't support geolocation";
+    outputResult(msg); // output error message
+    $('.pure-button').removeClass('pure-button-primary').addClass('pure-button-success'); // change button style
+  }
+
+  /*** 
+  requestLocation() returns a message, either the users coordinates, or an error message
+  **/
+  function requestLocation() {
+    /**
+    getCurrentPosition() below accepts 3 arguments:
+    a success callback (required), an error callback  (optional), and a set of options (optional)
+    **/
+
+    var options = {
+      // enableHighAccuracy = should the device take extra time or power to return a really accurate result, or should it give you the quick (but less accurate) answer?
+      enableHighAccuracy: false,
+      // timeout = how long does the device have, in milliseconds to return a result?
+      timeout: 5000,
+      // maximumAge = maximum age for a possible previously-cached position. 0 = must return the current position, not a prior cached position
+      maximumAge: 0
+    };
+
+    // call getCurrentPosition()
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+    // upon success, do this
+    function success(pos) {
+      // get longitude and latitude from the position object passed in
+      var lng = pos.coords.longitude;
+      var lat = pos.coords.latitude;
+      // and presto, we have the device's location!
+      msg = 'You appear to be at longitude: ' + lng + ' and latitude: ' + lat + '<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=480x480&maptype=roadmap&markers=color:red%7Clabel:A%7C' + lat + ',' + lng + '&sensor=false">';
+      outputResult(msg); // output message
+      $('.pure-button').removeClass('pure-button-primary').addClass('pure-button-success'); // change button style
+    }
+
+    // upon error, do this
+    function error(err) {
+      // return the error message
+      msg = 'Error: ' + err + ' :(';
+      outputResult(msg); // output button
+      $('.pure-button').removeClass('pure-button-primary').addClass('pure-button-error'); // change button style
+    }
+  } // end requestLocation();
+
+  /*** 
+  outputResult() inserts msg into the DOM  
+  **/
+  function outputResult(msg) {
+    $('.result').addClass('result').html(msg);
+  }
+} // end getLocation()
+
+// attach getLocation() to button click
+$('.pure-button').on('click', function() {
+  // show spinner while getlocation() does its thing
+  $('.result').html('<i class="fa fa-spinner fa-spin"></i>');
+  getLocation();
 });
-
-mymap.on('locationfound', onLocationFound);
-function onLocationFound(e) {
-    console.log(e); 
-    // e.heading will contain the user's heading (in degrees) if it's available, and if not it will be NaN. This would allow you to point a marker in the same direction the user is pointed. 
-    L.marker(e.latlng).addTo(mymap);
-}
 
 
 var horizontalLine = document.getElementById("horizontal");
