eb1b7043cae5b5f45e17f19b702f1d3b3c889c5f
camshaw1 <cameronshaw95@hotmail.com>
2016-05-02 04:29:07 +1200

    ,

--- js/javascript.js
+++ js/javascript.js
@@ -43,7 +43,7 @@ function getLocation() {
       var lng = pos.coords.longitude;
       var lat = pos.coords.latitude;
       // and presto, we have the device's location!
-      msg = 'You appear to be at longitude: ' + lng + ' and latitude: ' + lat + '<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=380x380&maptype=roadmap&markers=color:red%7Clabel:A%7C' + lat + ',' + lng + '&sensor=false">';
+      msg = 'You appear to be at longitude: ' + lng + ' and latitude: ' + lat + '<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=480x480&maptype=roadmap&markers=color:red%7Clabel:A%7C' + lat + ',' + lng + '&sensor=false">';
       outputResult(msg); // output message
       $('.pure-button').removeClass('pure-button-primary').addClass('pure-button-success'); // change button style
     }
