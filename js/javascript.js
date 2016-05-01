83ef6ab96489951462cc98b4a306966060160a92
camshaw1 <cameronshaw95@hotmail.com>
2016-05-02 00:57:48 +1200

    canvas

--- js/javascript.js
+++ js/javascript.js
@@ -468,34 +468,6 @@ function fingerprint_browser() {
   }
 }
 
-function fingerprint_canvas() {
-  "use strict";
-  var strOnError, canvas, strCText, strText, strOut;
-
-  strOnError = "Error";
-  canvas = null;
-  strCText = null;
-  strText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~1!2@3#4$5%6^7&8*9(0)-_=+[{]}|;:',<.>/?";
-  strOut = null;
-
-  try {
-    canvas = document.createElement('canvas');
-    strCText = canvas.getContext('2d');
-    strCText.textBaseline = "top";
-    strCText.font = "14px 'Arial'";
-    strCText.textBaseline = "alphabetic";
-    strCText.fillStyle = "#f60";
-    strCText.fillRect(125, 1, 62, 20);
-    strCText.fillStyle = "#069";
-    strCText.fillText(strText, 2, 15);
-    strCText.fillStyle = "rgba(102, 204, 0, 0.7)";
-    strCText.fillText(strText, 4, 17);
-    strOut = canvas.toDataURL();
-    return strOut;
-  } catch (err) {
-    return strOnError;
-  }
-}
 
 function fingerprint_connection() {
   "use strict";
@@ -1402,7 +1374,7 @@ var uid = fp.get();
 document.getElementById('fingerprint').innerHTML = uid;
 document.getElementById('browser').innerHTML = fingerprint_browser();
 document.getElementById('flash').innerHTML = fingerprint_flash();
-document.getElementById('canvas').innerHTML = fingerprint_canvas();
+//document.getElementById('canvas').innerHTML = fingerprint_canvas();//
 document.getElementById('connection').innerHTML = fingerprint_connection();
 document.getElementById('cookie').innerHTML = fingerprint_cookie();
 document.getElementById('display').innerHTML = fingerprint_display();
