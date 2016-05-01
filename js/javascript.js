e335305413bbea9f0997abff6c7c7378edeead2f
camshaw1 <cameronshaw95@hotmail.com>
2016-05-02 03:30:58 +1200

    ,ap

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,15 @@
 /** NOTE: uses jQuery for quick & easy DOM manipulation **/
 
-function getLocation(){
-  var msg; 
+function getLocation() {
+  var msg;
 
   /** 
   first, test for feature support
   **/
-  if('geolocation' in navigator){
+  if ('geolocation' in navigator) {
     // geolocation is supported :)
     requestLocation();
-  }else{
+  } else {
     // no geolocation :(
     msg = "Sorry, looks like your browser doesn't support geolocation";
     outputResult(msg); // output error message
@@ -19,12 +19,12 @@ function getLocation(){
   /*** 
   requestLocation() returns a message, either the users coordinates, or an error message
   **/
-  function requestLocation(){
+  function requestLocation() {
     /**
     getCurrentPosition() below accepts 3 arguments:
     a success callback (required), an error callback  (optional), and a set of options (optional)
     **/
-  
+
     var options = {
       // enableHighAccuracy = should the device take extra time or power to return a really accurate result, or should it give you the quick (but less accurate) answer?
       enableHighAccuracy: false,
@@ -33,40 +33,40 @@ function getLocation(){
       // maximumAge = maximum age for a possible previously-cached position. 0 = must return the current position, not a prior cached position
       maximumAge: 0
     };
-  
+
     // call getCurrentPosition()
-    navigator.geolocation.getCurrentPosition(success, error, options); 
-  
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
     // upon success, do this
-    function success(pos){
+    function success(pos) {
       // get longitude and latitude from the position object passed in
       var lng = pos.coords.longitude;
       var lat = pos.coords.latitude;
       // and presto, we have the device's location!
-      msg = 'Found you: ' + lng + ' and latitude: ' + lat  + '<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x600&maptype=roadmap&markers=color:red%7Clabel:A%7C' + lat + ',' + lng+ '&sensor=false">';
+      msg = 'You appear to be at longitude: ' + lng + ' and latitude: ' + lat + '<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=300x300&maptype=roadmap&markers=color:red%7Clabel:A%7C' + lat + ',' + lng + '&sensor=false">';
       outputResult(msg); // output message
       $('.pure-button').removeClass('pure-button-primary').addClass('pure-button-success'); // change button style
     }
-  
+
     // upon error, do this
-    function error(err){
+    function error(err) {
       // return the error message
       msg = 'Error: ' + err + ' :(';
       outputResult(msg); // output button
       $('.pure-button').removeClass('pure-button-primary').addClass('pure-button-error'); // change button style
-    }  
+    }
   } // end requestLocation();
 
   /*** 
   outputResult() inserts msg into the DOM  
   **/
-  function outputResult(msg){
+  function outputResult(msg) {
     $('.result').addClass('result').html(msg);
   }
 } // end getLocation()
 
 // attach getLocation() to button click
-$('.pure-button').on('click', function(){
+$('.pure-button').on('click', function() {
   // show spinner while getlocation() does its thing
   $('.result').html('<i class="fa fa-spinner fa-spin"></i>');
   getLocation();
